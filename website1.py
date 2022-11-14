b337c5cc5df8acf0b88731d0ed99331a6eeab782
corinneah <103472876+corinneah@users.noreply.github.com>
2022-11-14 17:48:15 -0500

    Update

--- website1.py
+++ website1.py
@@ -1,1 +1,43 @@
+#import packages 
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+#Getting the page 
+page = requests.get("https://www.zippia.com/tattoo-artist-jobs/demographics/")
+page
+page.status_code
+page.content
+
+#Create a BeautifulSoup object 
+soup = BeautifulSoup(page.text, 'html.parser')
+
+#Print Pretty 
+print(soup.prettify())
+
+# Get titles 
+titles = soup.find_all('h2', class_="brandonH2 w-730 z-mb-48")
+titles
+
+title_names = []
+
+# clean data a little 
+
+for i in titles:
+    t = i.text
+    t = t.strip()
+    t = t.replace('\n','')
+    t = t.replace(' ','')
+print(i.text)
+title_names.append(i.text)
+
+len(title_names)
+title_names
+
+# put into dataframe and get csv 
+df = pd.DataFrame({'Tattoo_Stats':title_names})
+
+df.to_csv('data/tattoo.csv')
+
+
 
--- website2.py
+++ website2.py
@@ -1,1 +1,40 @@
+#import packages 
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 
+#Getting the page 
+page = requests.get("https://www.pushsquare.com/guides/best-ps5-games")
+page
+page.status_code
+page.content
+
+#Create a BeautifulSoup object 
+soup = BeautifulSoup(page.text, 'html.parser')
+
+#Print Pretty 
+print(soup.prettify())
+
+# Get titles 
+Names = soup.find_all('h3', class_="heading")
+Names
+
+Name_list = []
+
+# clean data a little 
+
+for i in Names:
+    n = i.text
+    n = n.strip()
+    n = n.replace('\n','')
+    n = n.replace(' ','')
+print(i.text)
+Name_list.append(i.text)
+
+len(Name_list)
+Name_list
+
+# put into dataframe and get csv 
+df = pd.DataFrame({'Playstation_Games':Name_list})
+
+df.to_csv('data/PS5.csv')
